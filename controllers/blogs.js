0d9fee372a6fbfbf7bd24f0d04a1f295301daef8
kgni <admin@mdia.dk>
2022-07-09 14:33:19 +0200

    add exercise 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,18 +1,30 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (req, res) => {
-	const blogs = await Blog.find({});
+	const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
 	res.json(blogs);
 });
 
 blogsRouter.post('/', async (req, res) => {
-	const blog = new Blog(req.body);
+	const body = req.body;
 
-	const result = await blog.save();
+	const user = await User.findOne({});
 
-	res.status(201).json(result);
-	res.status(400);
+	const blog = new Blog({
+		title: body.title,
+		author: body.author,
+		url: body.url,
+		likes: 0,
+		user: user.id,
+	});
+
+	const savedBlog = await blog.save();
+
+	user.blogs = user.blogs.concat(savedBlog.id);
+	await user.save();
+	res.status(201).json(savedBlog);
 });
 
 blogsRouter.delete('/:id', async (req, res) => {
--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,11 @@ const usersRouter = require('express').Router();
 const User = require('../models/user');
 
 usersRouter.get('/', async (request, response) => {
-	const users = await User.find({});
+	const users = await User.find({}).populate('blogs', {
+		title: 1,
+		author: 1,
+		url: 1,
+	});
 
 	if (users.length === 0) {
 		response.status(404).json({ error: 'No users found' });
